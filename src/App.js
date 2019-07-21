67970705239c4e660afae3c11bc0127929dbfb18
Anthony Nguyen-Duong <aat2nguy@uwaterloo.ca>
2019-07-21 17:07:19 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Brevity</h1>
+        <Form className="upload-form" method="POST" action="http://localhost:8080/upload" enctype="multipart/form-data">
+          <Form.Group controlId="uploadFile">
+            <Form.Label>Upload PDF</Form.Label>
+            <Form.Control type="file" name="file" />
+          </Form.Group>
+          <Button variant="primary"type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
